e18d70cb49fb7b8c1daec0e3890ba54aeef2d37f
Raul Torrefiel Jr <aluroticooo@gmail.com>
2018-01-29 10:50:15 +0800

    Modify App to be a container Adds FilesList component placeholder

--- webapp/imports/ui/App.js
+++ webapp/imports/ui/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+
 import Login from './components/login';
 import LoginBox from './components/loginBox';
+import FilesList from './components/filesList';
 
-export default class App extends Component {
+class App extends Component {
   render() {
+    const { currentUser } = this.props;
     return (
       <div className="fm-container">
         <header>
@@ -12,7 +16,16 @@ export default class App extends Component {
 
         <LoginBox />
 
+        { this.props.currentUser ?
+          <FilesList />
+        : ''}
       </div>
     );
   }
 }
+
+export default withTracker(() => {
+  return {
+    currentUser: Meteor.user(),
+  };
+})(App);
---
+++ webapp/imports/ui/components/filesList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default class FilesList extends React.Component {
+  render() {
+    return (
+      <div className="files-list">
+        <div className="list-item">
+          <p>Lorem Ipsum</p>
+          <p>dolor amet</p> 
+        </div>
+      </div>
+    );
+  }
+}
