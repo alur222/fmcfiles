89b34610f6d9c021bc49837b997dea290d4381bd
Raul Torrefiel Jr <aluroticooo@gmail.com>
2018-01-29 11:54:29 +0800

    Add temporary file list placeholder

--- webapp/imports/ui/components/filesList.js
+++ webapp/imports/ui/components/filesList.js
@@ -5,9 +5,21 @@ export default class FilesList extends React.Component {
   render() {
     return (
       <div className="files-list">
-        <div className="list-item">
+        <div className="fm-list-item">
           <p>Lorem Ipsum</p>
-          <p>dolor amet</p> 
+          <p>dolor amet</p>
+        </div>
+        <div className="fm-list-item">
+          <p>Lorem Ipsum</p>
+          <p>dolor amet</p>
+        </div>
+        <div className="fm-list-item">
+          <p>Lorem Ipsum</p>
+          <p>dolor amet</p>
+        </div>
+        <div className="fm-list-item">
+          <p>Lorem Ipsum</p>
+          <p>dolor amet</p>
         </div>
       </div>
     );
